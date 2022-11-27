a502357288f32982024c5ce4b3767560ffe9f6b3
김재욱 <kjwook7522@gmail.com>
2022-11-27 17:36:36 +0900

    [main] base path 설정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import App from "@/App.svelte";
-import "@/normalize.scss";
-import "@/index.scss";
+import "./normalize.scss";
+import "./index.scss";
 
 const app = new App({
   target: document.getElementById("app"),
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 import type { ServerOptions, PluginOption } from "vite";
 
 const server: ServerOptions = {
@@ -10,7 +10,6 @@ const server: ServerOptions = {
 };
 
 const plugins: PluginOption = [
-  tsconfigPaths({}),
   svelte({
     preprocess: sveltePreprocess(),
   }),
@@ -19,10 +18,12 @@ const plugins: PluginOption = [
 export default defineConfig({
   plugins,
   server,
+  root: path.join(__dirname, "src"),
   css: {
     modules: {
       generateScopedName: "[local]___[hash:base64:4]",
     },
   },
+  resolve: { alias: { "@": "./" } },
   clearScreen: true,
 });
